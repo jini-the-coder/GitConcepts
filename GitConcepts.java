ea53d4da5a72fdfcde5eae72e4945f8ff37219d2
Jinisha <jinishakg11@gmail.com>
2023-01-10 17:49:02 +0530

    added printDuck method

--- GitConcepts.java
+++ GitConcepts.java
@@ -1,5 +1,10 @@
 public class GitConcepts{
     public static void main(String[] args){
         System.out.println("git concepts");
+        printDuck();
+    }
+
+    public static void printDuck(){
+        System.out.println("Duck");
     }
 }
